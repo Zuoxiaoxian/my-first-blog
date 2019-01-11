ae53884c422cee4255bb4c917bc29342abd2eeac
左孝贤 <Zxx1980179070@163.com>
2019-01-11 16:44:27 +0800

    add project

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+# 创建一个博客文章模型
+
+from django.utils import timezone
+
+class Post(models.Model):
+    # models.ForeignKey - 这是指向另一个模型的连接。
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now
+    )
+    published_date = models.DateTimeField(
+        blank=True, null=True
+    )
+
+    def publish(self):
+        self.publish_date = timezone.now()
+        self.save()
+    
+    def __str__(self):
+        return self.title
