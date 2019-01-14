587d2e4104a763a22d109ceb7dce017b50719255
左孝贤 <Zxx1980179070@163.com>
2019-01-14 09:24:20 +0800

    url、templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'', include('blog.urls')), # 将  'http://127.0.0.1:8000/' 请求转到blog.urls， 并看看那里面有没有进一步的指示
     url(r'^admin/', include(admin.site.urls)),
 ]
